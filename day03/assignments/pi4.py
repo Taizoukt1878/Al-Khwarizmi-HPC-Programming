1d0d86a70ca1ec1af39441d02568722eae4b607b
Anouar Taizoukt <taizouktanouar@gmail.com>
2022-07-09 16:09:33 +0100

    update0907

--- day03/assignments/pi4.py
+++ day03/assignments/pi4.py
@@ -4,6 +4,7 @@ from pyccel.decorators import types
 def pi(n:'int'):
 #     print("Execution of PI in parallel with", nb_tasks,"threads")
 
+    #$ omp parallel
     h = float(1/n)
     for k in range(100):
         Pi_calc = 0.
@@ -11,6 +12,9 @@ def pi(n:'int'):
             x = h * (i + 0.5)
             Pi_calc += 4.0 / (1. + x * x)
 
-
+        # omp atomic
         Pi_calc = h * Pi_calc
+        
+    #omp end parallel
+    
     return Pi_calc
